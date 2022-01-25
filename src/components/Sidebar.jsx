44cda7901dddd153b1f257bd66a03125718f6851
Ziabeher Phillips <zabq02@gmail.com>
2022-01-25 17:06:17 -0400

    Updated Sidebar UI

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -11,8 +11,8 @@ const Sidebar = () => {
         <>
         <div className={` ${menuContext.toggle ? menuContext.context : '-translate-x-full'} absolute h-4/5 bg-white w-1/5 flex flex-col justify-around shadow rounded-r-lg transition`}>
             <div className="flex items-center text-red text-lg space-x-3 justify-center">
-            <i className="fas fa-heart"></i>
-            <h1>Your Favorites</h1>
+            <i class="fas fa-star"></i>
+            <h1>Trending</h1>
             </div>
 
             <div className="flex items-center text-red text-lg space-x-3 justify-center">
@@ -21,13 +21,8 @@ const Sidebar = () => {
             </div>
 
             <div className="flex items-center text-red text-lg space-x-3 justify-center">
-            <i className="fas fa-heart"></i>
-            <h1>Your Favorites</h1>
-            </div>
-
-            <div className="flex items-center text-red text-lg space-x-3 justify-center">
-            <i className="fas fa-heart"></i>
-            <h1>Your Favorites</h1>
+            <i class="fas fa-book-open"></i>
+            <h1>About</h1>
             </div>
         </div>
         </>
