4a0cb28266cf1492787b99754e0f52710a5d8cff
Ziabeher Phillips <zabq02@gmail.com>
2022-01-26 11:42:15 -0400

    UI improvements

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -14,6 +14,10 @@ const Sidebar = () => {
         <>
         <div className={` ${menuContext.toggle ? menuContext.context : '-translate-x-full'} absolute h-4/5 bg-white w-1/5 flex flex-col justify-around shadow rounded-r-lg transition`}>
             <div className="flex items-center text-red text-lg space-x-3 justify-center">
+            <i class="fas fa-home"></i>
+            <Link to={"/"}><h1>Home</h1></Link>
+            </div>
+            <div className="flex items-center text-red text-lg space-x-3 justify-center">
             <i className="fas fa-star"></i>
             <h1>Trending</h1>
             </div>
--- src/pages/FavDishes.jsx
+++ src/pages/FavDishes.jsx
@@ -7,7 +7,9 @@ const FavDishes = () => {
     const [favDishes, setFavDishes] = useContext(FavoriteDishContext)
     console.log(favDishes)
 
-  return <div className="container grid grid-cols-4 gap-5 mt-10">
+  return  <>
+    <h1 className="text-center text-7xl mt-10 font-nautigal text-red">Your Favorite Dishes!</h1>
+    <div className="container grid grid-cols-4 gap-5 mt-10">
       {favDishes.map((dish) => (
     <div className="text-center rounded w-full shadow-2xl pb-5" key={dish.label}>
     <img className="w-screen" src={dish.image} alt="" />
@@ -17,6 +19,7 @@ const FavDishes = () => {
     </div>
 ))}
   </div>
+  </>
 };
 
 export default FavDishes;
