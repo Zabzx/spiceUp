cf087ccf9604a66c6938d55e5559a1d041894033
Ziabeher Phillips <zabq02@gmail.com>
2022-02-07 14:28:57 -0400

    fixed minor search UI bug

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -40,7 +40,7 @@ const Featured = () => {
     return (
         <>
         <FavAlert/>
-        {!searched ? <h1 className="text-center text-7xl mt-28 font-nautigal text-red">Featured Ingredient: Cheese</h1> : <h1 className="text-center text-7xl mt-10 font-nautigal text-red">Results found for {searchValue}</h1>}
+        {!searched ? <h1 className="text-center text-7xl mt-28 font-nautigal text-red">Featured Ingredient: Cheese</h1> : <h1 className="text-center text-7xl mt-28 font-nautigal text-red">Results found for {searchValue}</h1>}
         <div className="container grid grid-cols-3 gap-10 mt-10 mb-10">
             {!loadingData ? featuredData.map((item) => (
                 <div className="border-gray border-xl text-center rounded-xl w-full bg-slate pb-5" key={item.recipe.image}>
