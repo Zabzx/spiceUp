9fedd7967f823afa22070476436304080abe3330
Ziabeher Phillips <zabq02@gmail.com>
2022-02-16 12:43:01 -0400

    trying to fix the footer

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -10,15 +10,12 @@ const About = () => {
     setMenu({...menu, toggle: false})
   }, [])
   
-  return <>
-  <div className="flex flex-col h-screen justify-between">
-    <div className="container">
+  return (<>
+  <div className="container">
   <h1 className="text-5xl mt-28">This is my first Official Project using Tailwind CSS</h1>
       <p className="mt-5">It was a bit of a challenge to get Tailwind working with React properly but I managed to get the hang of things eventually. The API used for this project unfortunately did not provide as much data as I'd hope. This is the reason the site may feel a bit on the empty side. Regardless, I did what I can with what was provided and developed some basic functionality using the Context API with react hooks and Axios for fetching URL Requests. After getting used to TailwindCSS, I can safely say that I really enjoy the developer experience with it and will definately be using this for my future and better projects.</p>
       </div>
-      <Footer/>
-      </div>
      </>
-  };
+  )};
 
 export default About;
